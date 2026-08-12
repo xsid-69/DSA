7d621aef4863f1e792e28550a90a5cb60974e22f
Siddhant Wankhade <siddhantwankhade069@gmail.com>
2026-08-12 20:20:05 +0530

    Time: 1 ms (86.61%), Space: 56.3 MB (47.94%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 0) {
+
+            let isLeftEmpty = (i === 0 || flowerbed[i - 1] === 0);
+            let isRightEmpty = (i === flowerbed.length - 1 || flowerbed[i + 1] === 0);
+
+            if (isLeftEmpty && isRightEmpty) {
+                flowerbed[i] = 1; 
+                n--;
+                if (n <= 0) return true;
+            }
+        }
+    }
+    
+    return n <= 0;
+};
