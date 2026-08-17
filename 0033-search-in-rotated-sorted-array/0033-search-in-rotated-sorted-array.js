10c052db8bbf6e53d8e28af904ffaa6860471741
Siddhant Wankhade <siddhantwankhade069@gmail.com>
2026-08-17 18:13:13 +0530

    Time: 0 ms (100%), Space: 53.7 MB (68.39%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start =0 , end = nums.length-1;
+    while(start<=end){
+        let mid = Math.floor((start+end)/2);
+        if(nums[mid] == target)return mid;
+        if(nums[start]<=nums[mid]){
+            if(target>=nums[start] && target<=nums[mid]){
+                end =mid-1 ;
+            }else{
+                start= mid+1;
+            }
+        }else{
+            if(target>=nums[mid] && target<=nums[end]){
+                start=mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+    }
+    return -1;
+};
