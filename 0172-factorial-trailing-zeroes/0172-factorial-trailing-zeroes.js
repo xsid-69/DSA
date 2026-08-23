568610e5f44faf5c3bc87097afc548606c4e8142
Siddhant Wankhade <siddhantwankhade069@gmail.com>
2026-08-23 20:49:10 +0530

    Time: 0 ms (100%), Space: 53.8 MB (75%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+    let count = 0;
+    
+    while (n > 0) {
+        n = Math.floor(n / 5);
+        count += n;
+    }
+    
+    return count;
+};
